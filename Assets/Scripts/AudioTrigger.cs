a615dabed6a449396f1302702ee6ceed714b3274
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2020-12-09 23:56:08 +0100

    Add sounds inside and outside
    Add transition between snapshots

---
+++ Assets/Scripts/AudioTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioTrigger : MonoBehaviour
+{
+    [SerializeField] private AudioMixer _audioMixer;
+    public bool isTriggeringInside = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (isTriggeringInside)
+            {
+                _audioMixer.FindSnapshot("Inside").TransitionTo(1f);
+            }
+            else
+            {
+                _audioMixer.FindSnapshot("Outside").TransitionTo(1f);
+            }
+        }
+    }
+}
